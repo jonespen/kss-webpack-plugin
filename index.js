e2736247a51f06f9d38208ee189f249f40a46ff7
Dave Cranwell <dave.cranwell@adestra.com>
2018-01-03 17:27:00 +0000

    Adds support for a 'chunk' option in which user specifies which webpack chunks to add to generated styleguide by default

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var kss = require('kss');
+var path = require('path');
 
 function KssPlugin(options) {
   if (!options) {
@@ -7,16 +8,62 @@ function KssPlugin(options) {
   if (!options.source) {
     throw 'kss webpack plugin: source is not defined';
   }
+  if(!options.chunks) {
+    throw 'kss webpack plugin: chunks not defined'
+  }
   this.options = options;
 }
 
 KssPlugin.prototype.apply = function (compiler) {
+  compiler.plugin('emit', (compilation, callback) => {
+    this.render(compilation, callback);
+  });
+};
+
+KssPlugin.prototype.render = function (compilation, callback) {
   var self = this;
-  compiler.plugin('done', function() {
-    kss(self.options, function (error) {
+  new Promise(function(resolve, reject) {
+    var assets = this._buildAssets(compilation);
+  
+    kss(Object.assign({}, self.options, assets), function (error) {
       if (error) throw error;
     });
+    resolve();
+  }).then(() => {
+      callback();
+    }
+  )
+};
+
+KssPlugin.prototype._buildAssets = function (compilation) {
+  const assets = {
+    css: [],
+    js: []
+  };
+
+  this.options.chunks.forEach(function(key) {
+    var pushAsset = function(file) {
+      const ext = path.extname(file);
+
+      if (ext === '.css') {
+          assets.css.push(path.join('/', file))
+      } else if (ext === '.js') {
+          assets.js.push(path.join('/', file));
+      }
+    }   
+
+    const asset = compilation.getStats().toJson().assetsByChunkName[key];
+
+    if(typeof asset === 'string') {
+      pushAsset(asset)
+    } else {
+      asset.forEach((file) => {
+        pushAsset(file)
+      })
+    }
   });
+
+  return assets;
 };
 
 module.exports = KssPlugin;
